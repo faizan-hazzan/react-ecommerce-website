386befc05264a7b0ad534c95a30957389d6c9782
hasan-student-com <f034303129@gmail.com>
2026-03-03 17:17:09 +0500

    Added Home Page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import ProductCard from "../components/ProductCard"
+import { getProducts } from '../data/products'
+
+
+export default function Home (){
+
+    const products= getProducts()
+
+    return (
+    <div className="page"> 
+        <div className="home-hero">
+            <h1 className="home-title">Welcome to ShopHub</h1>
+            <p className="home-subtitle">Discover Amazing products at great prices</p>
+        </div>
+        <div className="container">
+            <h2 className="page-title">Our Products</h2>
+            <div className="product-grid">
+                {products.map((product) => 
+                   <ProductCard product={product}/>
+                )}
+            </div>
+        </div>
+    </div>
+    )
+}
